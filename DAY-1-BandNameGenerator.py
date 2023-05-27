365d36ae95c018ab8d8ac1f39a6a7384ef6fda59
Elida Arnaut <elida.arnaut@gmail.com>
2023-05-27 16:24:28 +0200

    Day 1 - Band Name Generator upload

---
+++ DAY-1-BandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator! This is the end result of our day 1 of a 100 days of code challenge.\n")
+city = input("What is the name of the city you grew up in? \n")
+pet_name = input("What is the name of your pet? \n")
+band_name = city + " " + pet_name
+print("Your band name could be " + band_name)
